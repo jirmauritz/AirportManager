6a7fd3cc2a6d802ee2d05a8adba84f3d1818ee86
Tomas Valka <tomas.valka@gmail.com>
2015-10-20 19:24:01 +0200

    + POM, basic structure

---
+++ src/main/java/cz/muni/fi/pa165/dao/AirplaneDao.java
@@ -0,0 +1,8 @@
+package cz.muni.fi.pa165.dao;
+
+/**
+ * @author Tomas Valka
+ * @author 422718@mail.muni.cz
+ */
+public class AirplaneDao {
+}
---
+++ src/main/java/cz/muni/fi/pa165/entity/Flight.java
@@ -0,0 +1,8 @@
+package cz.muni.fi.pa165.entity;
+
+/**
+ * @author Tomas Valka
+ * @author 422718@mail.muni.cz
+ */
+public class Flight {
+}
