947f248f3285de502909dc0d86f5ed9ae1469677
Lenka Heldová <lenkaheldova@gmail.com>
2015-10-26 22:23:45 +0100

    Addition of crud operations and removal of using Spring Data.

--- src/main/java/cz/muni/fi/pa165/dao/StewardDao.java
+++ src/main/java/cz/muni/fi/pa165/dao/StewardDao.java
@@ -1,35 +1,40 @@
 package cz.muni.fi.pa165.dao;
 
 import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
-
 import cz.muni.fi.pa165.entity.Steward;
 
-
 /**
- * Repository for entity Steward, using Spring Data.
+ * Repository for entity Steward.
  * 
  * @author Lenka Heldova
  * @author 422578@mail.muni.cz
  *
  */
-public interface StewardDao extends CrudRepository<Steward, Long> {
+public interface StewardDao {
 
 	/**
 	 * Finds list of stewards with given first name
 	 * 
 	 * @param firstName
-	 * @return list of stewards 
+	 * @return list of stewards
 	 */
 	List<Steward> findByFirstName(String firstName);
-	
+
 	/**
 	 * Finds list of stewards with given last name
 	 * 
 	 * @param lastName
-	 * @return list of stewards 
+	 * @return list of stewards
 	 */
 	List<Steward> findByLastName(String lastName);
-	
+
+	void create(Steward steward);
+
+	Steward findById(Long id);
+
+	List<Steward> findAll();
+
+	void update(Steward steward);
+
+	void delete(Steward steward);
 }
