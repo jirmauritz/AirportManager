7aff762c9021095219a5d07bc236e669c6399e19
Jiří Mauritz <jirmauritz@gmail.com>
2015-10-29 11:14:14 +0100

    Switch List for Set, remove Lombok, fixed JPQL queries

--- src/main/java/cz/muni/fi/pa165/dao/FlightDao.java
+++ src/main/java/cz/muni/fi/pa165/dao/FlightDao.java
@@ -18,7 +18,6 @@ public interface FlightDao {
      * Persists specified flight in the database.
      *
      * @param flight entity to persist
-	 * @throws NullPointerException if the specified entity is null
      */
     void create(Flight flight) throws NullPointerException;
 
@@ -26,7 +25,6 @@ public interface FlightDao {
      * Updates the state of the specified entity in the database. The entity must have attribute id set.
      *
      * @param flight entity to update
-     * @throws NullPointerException if the specified entity is null
      */
     void update(Flight flight) throws NullPointerException;
 
@@ -34,7 +32,6 @@ public interface FlightDao {
      * Deletes the specified entity from the database. The entity must have attribute id set.
      *
      * @param flight entity to delete
-     * @throws NullPointerException if the specified entity is null
      */
     void delete(Flight flight) throws NullPointerException;
 	
@@ -43,7 +40,6 @@ public interface FlightDao {
      *
      * @param id an id to look for
      * @return Flight with specified id or null
-     * @throws NullPointerException if the specified id is null
      */
     Flight findById(Long id) throws NullPointerException;
 
--- src/main/java/cz/muni/fi/pa165/entity/Destination.java
+++ src/main/java/cz/muni/fi/pa165/entity/Destination.java
@@ -7,15 +7,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-@Getter
-@Setter
-@ToString
-@NoArgsConstructor
 
 /**
  * Class represents location of an airport, characterized by its code, city and country.
@@ -34,14 +25,56 @@ public class Destination {
 	@Column(nullable=false,unique=true)
 	private String name; // airport code
 	
+	@NotNull
 	private String city;
 	
+	@NotNull
 	private String country;
 
 	public Destination(String name, String city, String country) {
 		this.name = name;
 		this.city = city;
 		this.country = country;
+	}
+
+	public Destination() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	@Override
+	public String toString() {
+		return "Destination{" + "id=" + id + ", name=" + name + ", city=" + city + ", country=" + country + '}';
 	}
 	
 	@Override
