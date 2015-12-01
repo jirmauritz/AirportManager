43d212fffb5648a38e38d508ffffac0b27f09daa
tomas.valka <tomas.valka@mycroftmind.com>
2015-12-01 17:17:36 +0100

    * Maven multimodule change fot milestone 3

---
+++ AirportManager UI/src/main/java/cz/muni/fi/pa165/airport_manager/config/UIConfiguration.java
@@ -0,0 +1,10 @@
+package cz.muni.fi.pa165.airport_manager.config;
+
+/**
+ *
+ *
+ * @author Tomas Valka
+ * @author 422718@mail.muni.cz
+ */
+public class UIConfiguration {
+}
---
+++ AirportManager UI/src/main/java/cz/muni/fi/pa165/airport_manager/security/ProtectFilter.java
@@ -0,0 +1,29 @@
+package cz.muni.fi.pa165.airport_manager.security;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Filter user access
+ *
+ * @author Tomas Valka
+ * @author 422718@mail.muni.cz
+ */
+public class ProtectFilter implements Filter {
+
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
